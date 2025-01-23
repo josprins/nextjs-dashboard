041de67b8f2ed5de687855be50b70d1829b0a9bc
josprins <35664057+josprins@users.noreply.github.com>
2025-01-23 16:03:14 +0100

    added customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,34 @@
-const CustomersPage = () => {
-  return <p>Customers Page</p>;
-};
+import CostumersTable from "@/app/ui/customers/table";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 
-export default CustomersPage;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+
+  console.log(customers);
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Create Invoice",
+            href: "/dashboard/invoices/create",
+            active: true,
+          },
+        ]}
+      />
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <CostumersTable customers={customers} />
+      </Suspense>
+    </main>
+  );
+}
