6a57c3b5e9c773b228cea00c2562d8f66c119902
josprins <35664057+josprins@users.noreply.github.com>
2025-01-24 09:32:32 +0100

    fix eslint & errors/not-found

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -2,7 +2,7 @@ import { roboto } from "@/app/ui/fonts";
 import {
   AtSymbolIcon,
   KeyIcon,
-  ExclamationCircleIcon,
+  // ExclamationCircleIcon,
 } from "@heroicons/react/24/outline";
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "./button";
